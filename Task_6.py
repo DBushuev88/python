ea5387751f604f58a9bf4695e612295efa48619b
Dmitriy Bushuev <psilocyb20@gmail.com>
2020-07-13 20:41:50 +0300

    Add files via upload

---
+++ Task_6.py
@@ -0,0 +1,6 @@
+subj = {}
+with open("text_6.txt", "r", encoding="utf-8") as init_f:
+    for line in init_f:
+        subject, lecture, practice, lab = line.split()
+        subj[subject] = str(lecture) + str(practice) + str(lab)
+    print(f"Общее количество часов по предмету - \n {subj}")
